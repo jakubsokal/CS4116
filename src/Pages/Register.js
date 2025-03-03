c97f91829eb4efedb01a55efc16ad39de265aa3b
Kevin Burke <22355634@studentmail.ul.ie>
2025-03-03 20:05:18 +0000

    Removed unused code and added register page

---
+++ src/Pages/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterForm from "../Components/RegisterForm/RegisterForm"; 
+
+const Register = () => {
+  return (
+    <div className="register-page">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
