cfc56935cd60df6621cea125e16ac133622ecfcd
Jakub.Sokal <22372377@studentmail.ul.ie>
2025-04-24 14:36:29 +0100

    removing unused code

--- src/app/inquiry/page.js
+++ src/app/inquiry/page.js
@@ -2,19 +2,7 @@
 import AdminTable from "@/components/InquiryTable";  
 import Navbar from "@/components/Navbar";
 
-async function fetchInquiries() {
-  const res = await fetch(`/api/inquiries`, {
-    cache: "no-store", 
-  });
-
-  if (!res.ok) throw new Error("Failed to fetch inquiries");
-
-  return res.json(); 
-}
-
 export default async function Page() {
-  const reports = await fetchInquiries(); 
-
   return (
     <div>
       <Navbar />
